f5205c761c62ac12e363bba4fe553ba2a086826b
Teodoro <biel.teodorob@gmail.com>
2024-08-19 17:50:26 -0300

    test: add CreateUserUseCase

---
+++ src/domain/user/use-cases/create-user.spec.ts
@@ -0,0 +1,58 @@
+import { InMemoryUserRepository } from '../../../../test/repositories/in-memory-user-repository'
+import { HashRepository } from '../services/hash-repository'
+import { CreateUserUseCase } from './create-user'
+import { HashSimulator } from '../../../../test/services/hash-simulator'
+
+let userRepository: InMemoryUserRepository
+let hashRepository: HashRepository
+let useCase: CreateUserUseCase
+describe('CreateUserUseCase', () => {
+	beforeEach(() => {
+		userRepository = new InMemoryUserRepository()
+		hashRepository = new HashSimulator()
+		useCase = new CreateUserUseCase(userRepository, hashRepository)
+	})
+	test('Should create a user.', async () => {
+		const user = await useCase.handle({
+			email: 'valid_email@email.com',
+			name: 'valid_name',
+			password: 'valid_password'
+		})
+
+		const hashedPassword = await hashRepository.hash('valid_password')
+
+		expect(user.isRight()).toBe(true)
+		expect(user.value).toHaveProperty('id')
+		expect(userRepository.items[0].password).toBe(hashedPassword)
+		expect(userRepository.items[0].name).toBe('valid_name')
+		expect(userRepository.items[0].email.value).toBe(
+			'valid_email@email.com'
+		)
+	})
+
+	test('Should not create a user with an already registered email.', async () => {
+		await useCase.handle({
+			email: 'valid_email@email.com',
+			name: 'valid_name',
+			password: 'valid_password'
+		})
+
+		const user2 = await useCase.handle({
+			email: 'valid_email@email.com',
+			name: 'valid_name',
+			password: 'valid_password'
+		})
+
+		expect(user2.isLeft()).toBe(true)
+	})
+
+	test('Should not create a user with an invalid email.', async () => {
+		const user = await useCase.handle({
+			email: 'invalid_email@email',
+			name: 'valid_name',
+			password: 'valid_password'
+		})
+
+		expect(user.isLeft()).toBe(true)
+	})
+})

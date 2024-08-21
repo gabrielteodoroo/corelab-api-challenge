3ba3b033587950e91302a7c8b73fed430a9936e8
Teodoro <biel.teodorob@gmail.com>
2024-08-21 17:22:08 -0300

    feat: Add the Prisma model User and model Note.

---
+++ prisma/migrations/20240821201941_create_table_note/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "notes" (
+    "id" UUID NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "is_favorite" BOOLEAN NOT NULL,
+    "user_id" UUID NOT NULL,
+
+    CONSTRAINT "notes_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "notes" ADD CONSTRAINT "notes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

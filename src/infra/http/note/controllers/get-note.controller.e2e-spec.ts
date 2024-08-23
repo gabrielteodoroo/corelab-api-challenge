49c2349cd71582e471188a3b9cfe7df42f216f9c
Teodoro <biel.teodorob@gmail.com>
2024-08-23 11:25:26 -0300

    test: Get note controller

---
+++ src/infra/http/note/controllers/get-note.controller.e2e-spec.ts
@@ -0,0 +1,69 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication, ValidationPipe } from '@nestjs/common'
+import * as request from 'supertest'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { AppModule } from '@/app.module'
+import { JwtService } from '@nestjs/jwt'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { hash } from 'bcrypt'
+import { randomUUID } from 'crypto'
+
+describe('GetNoteController (e2e)', () => {
+	let app: INestApplication
+	let prisma: PrismaService
+	let jwt: JwtService
+
+	let user: any
+	let note: any
+
+	beforeAll(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule, DatabaseModule]
+		}).compile()
+
+		app = moduleFixture.createNestApplication()
+		app.useGlobalPipes(new ValidationPipe())
+		prisma = moduleFixture.get(PrismaService)
+		jwt = moduleFixture.get(JwtService)
+
+		await app.init()
+
+		user = {
+			id: randomUUID(),
+			name: 'Gabriel teodoro',
+			email: 'gabriel@email.com',
+			password: await hash('gabriel123', 10)
+		}
+
+		await prisma.user.create({
+			data: user
+		})
+
+		note = {
+			id: randomUUID(),
+			title: 'Note title',
+			text: 'Note text',
+			userId: user.id,
+			color: '#ffffff',
+			createdAt: new Date(),
+			updatedAt: new Date(),
+			isFavorite: false
+		}
+
+		await prisma.note.create({
+			data: note
+		})
+	})
+
+	test('/notes/:id (GET)', async () => {
+		const token = jwt.sign(user)
+
+		const response = await request(app.getHttpServer())
+			.get(`/notes/${note.id}`)
+			.set('Authorization', `Bearer ${token}`)
+			.send()
+
+		expect(response.statusCode).toBe(200)
+		expect(response.body).toHaveProperty('id')
+	})
+})
